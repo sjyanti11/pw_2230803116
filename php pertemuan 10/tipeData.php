985acd2d37dafeb5c27f45ead5b71b3c7be87f9a
sjyanti11 <146347316+sjyanti11@users.noreply.github.com>
2023-11-15 08:45:44 +0700

    Add files via upload

---
+++ php pertemuan 10/tipeData.php
@@ -0,0 +1,14 @@
+<?php
+
+$nrp = "173040888";
+$nama = "lorem ipsum";
+$umur = "20";
+$nilai = "75.75";
+
+echo "Nrp  :". $nrp."</br>";
+echo "Nama : $nama</br>";
+print "Umur : ".$umur;print "<b/br>";
+
+printf ("Nilai : %.3f</br>", $nilai);
+
+?>
---
+++ php pertemuan 10/variabel.php
@@ -0,0 +1,19 @@
+<?php
+
+$a ="hello";
+$hello = "Hello World";
+
+// menampilkan isi variabel $a
+// hello
+echo $a . "</br>";
+
+//menampilkan isi variabel $a
+// Hello World!
+echo $hello . "</br>";
+
+
+
+
+echo $$a . "</br>";
+
+?>
